8fb3fc9ee5ea9ef55947137927c0c0b86d620681
ailinbaudin <ailinbaudin@gmail.com>
2025-04-04 20:09:28 -0300

    edit-the-file-vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: "/shock-wave-repair-shop/", 
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,6 +19,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist",
+    outDir: "dist", 
   },
 }));
